73447f978b48b5e9374d5659b87cbef0184a4f80
Amit-Tomar <amit.tomar@iiitb.org>
2020-01-29 22:59:46 +0530

    Updates

---
+++ precache-manifest.ecebed8ae5e50507b5a53a36dcbbd77a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48583f00cb50d2087dc76c35ce3be53b",
+    "url": "/p5-learning/index.html"
+  },
+  {
+    "revision": "bce3c54e753cb3159d09",
+    "url": "/p5-learning/static/css/main.00be4956.chunk.css"
+  },
+  {
+    "revision": "63a20fb9971ef6d6a7bf",
+    "url": "/p5-learning/static/js/2.66c2c16c.chunk.js"
+  },
+  {
+    "revision": "271e68a9afaf321badd939e15e4e9028",
+    "url": "/p5-learning/static/js/2.66c2c16c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bce3c54e753cb3159d09",
+    "url": "/p5-learning/static/js/main.9b859ae5.chunk.js"
+  },
+  {
+    "revision": "9f8be5d8208b0ea3986e",
+    "url": "/p5-learning/static/js/runtime-main.44098ba3.js"
+  }
+]);
